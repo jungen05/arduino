2b9854a092c97f1a69f69815aa23c294d8476bd3
jungen05 <jungen831@gmail.com>
2025-09-16 09:32:28 +0900

    Add files via upload

---
+++ 04_example_3.ino
@@ -0,0 +1,25 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); //Initialize serial port
+  while (!Serial) {
+    ;//wait for serial port to connect
+  }
+  Serial.println("Hello World!");
+  count=toggle=0;
+  digitalWrite(PIN_LED, toggle); //turn off LED.
+
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle=toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
+
+int toggle_state(int state) {
+  return !state; 
+}
